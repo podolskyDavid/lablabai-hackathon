9d90487fe90748dbbd6160869f626a00c551851e
Alex Pokras <a.a.pokras@gmail.com>
2023-08-19 14:58:09 +0200

    Added file URLs to the database

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -1,12 +1,23 @@
 from supabase import create_client, Client
+from task import Task
+from pandas import DataFrame
+
 
 url: str = "https://eiruqjgfkgoknuhihfha.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpcnVxamdma2dva251aGloZmhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI0MzQxNTcsImV4cCI6MjAwODAxMDE1N30.HKZHbuiB2r8NN367J0LkD2UgwhaqJS2f0Ux9ezCFETA"
 supabase: Client = create_client(url, key)
 table = supabase.table("db_steps")
-# bucket = supabase.storage.get_bucket("bucket_steps")
-bucket = None
+bucket = supabase.storage.from_('bucket_steps')
+
 
 def client():
     return supabase
 
+def new_task(user_id: str, initial_df: DataFrame, initial_df_frontend: DataFrame | None = None):
+    """ Create a new task for a new initial_df DataFrame or retrieve an existing task with task_id """
+    return Task(user_id, None, initial_df, initial_df_frontend)
+
+
+def get_task(user_id: str, task_id: int):
+    """ Retrieve an existing task with task_id """
+    return Task(user_id, task_id)
--- api/database/task.py
+++ api/database/task.py
@@ -1,5 +1,7 @@
-from __init__ import table, bucket
+from __init__ import supabase, table, bucket
 from typing import *
+import os
+import re
 from pandas import DataFrame
 
 
@@ -9,7 +11,7 @@ class Task:
     max_step_count: int
     def __init__(self, user_id: str, task_id: int | None = None,
                  initial_df: DataFrame | None = None, initial_df_frontend: DataFrame | None = None):
-        """ Create a new DataFrame cleaning task """
+        """ Create a new task for a new initial_df DataFrame or retrieve an existing task with task_id """
         self.user_id = user_id
         self.task_id = task_id
 
@@ -24,28 +26,54 @@ class Task:
                 raise ValueError('initial_df must be provided when creating a new task, i.e. when task_id is None')
             if initial_df_frontend is None:
                 initial_df_frontend = initial_df
-            self.new_step('', 'Initial DataFrame', initial_df, initial_df_frontend)
+            self.upload_new_step('', 'Initial DataFrame', initial_df, initial_df_frontend)
         else:
             # find max step id within an existing task
             step_counts: List = table.select('task_id, step_count').eq('task_id', self.task_id).execute().data
             self.max_step_count = max([int(c['step_count']) for c in step_counts])
 
-    def new_step(self, transformation: str, explanation: str, df_after: DataFrame, df_frontend: DataFrame):
-        """ Create a new step in the task """
-        assert isinstance(self.max_step_count, int)
+    def upload_new_step(self, transformation: str, explanation: str, df_after: DataFrame, df_frontend: DataFrame):
+        """ Upload a new step in the task to the DB """
         self.max_step_count += 1
+        step_id = str(self.task_id) + '_' + str(self.max_step_count)
+
+        # convert dataframes to csv
+        os.makedirs('temp', exist_ok=True)
+        df_after.to_csv('temp/df.csv', index=False)
+        with open('temp/df.csv', 'rb') as f:
+            filepath = f'{self.user_id}/df_after_{step_id}.csv'
+            bucket.upload(filepath, f)
+            df_after_url = bucket.get_public_url(filepath)
+        df_frontend.to_csv('temp/df.csv', index=False)
+        with open('temp/df.csv', 'rb') as f:
+            filepath = f'{self.user_id}/df_frontend_{step_id}.csv'
+            bucket.upload(filepath, f)
+            df_frontend_url = bucket.get_public_url(filepath)
+        os.remove('temp/df.csv')
+
         table.insert({
-            'step_id': str(self.task_id) + '_' + str(self.max_step_count),
+            'step_id': step_id,
             'task_id': self.task_id,
             'step_count': self.max_step_count,
             'user_id': self.user_id,
             'transformation': transformation,
             'explanation': explanation,
+            'df_after': df_after_url,
+            'df_frontend': df_frontend_url
         }).execute()
 
 
 if __name__ == '__main__':
-    # task = Task(user_id='test@example.com', initial_df=DataFrame({'a': [1, 2, 3]}))
+    my_df = DataFrame({'a': [1, 2, 3]})
+    # task = Task(user_id='test@example.com', initial_df=my_df, initial_df_frontend=my_df)
     task = Task(user_id='nico@example.com', task_id=1)
-    task.new_step('lambda x: x', 'test transformation - no change', None, None)
-    print(task.task_id, task.user_id, task.max_step_count)
+    task.upload_new_step('lambda x: x', 'test transformation 1 - no change', my_df, my_df)
+    task.upload_new_step('lambda x: x', 'test transformation 2 - no change', my_df, my_df)
+    task.upload_new_step('lambda x: x', 'test transformation 3 - no change', my_df, my_df)
+    # print(task.task_id, task.user_id, task.max_step_count)
+
+    # with open('test_csv/test.csv', 'rb') as f:
+    #     res = bucket.upload('test3.csv', f)
+    #     print(res)
+    #     print(bucket.get_public_url('test1.csv'))
+
